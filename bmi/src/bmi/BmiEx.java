c86b737559d5f0e47eabffc655e2193b42fa5a65
user <user@DESKTOP-2PGRMF4>
2023-03-17 12:26:46 +0900

    플젝 신규 생성

---
+++ bmi/src/bmi/BmiEx.java
@@ -0,0 +1,21 @@
+package bmi;
+
+public class BmiEx {
+
+	public static void main(String[] args) {
+		
+		int weight = 55;
+		double height = 1.6;
+		double bmi= weight /(height*height);
+		
+		
+		
+		System.out.println("환영합니다.");		
+		System.out.printf("당신의 BMI는 %.1f 입니다.",bmi);
+		
+		
+		
+
+	}
+
+}
