a3389902c5a747a7e4c99088767034f9bf174410
user <user@DESKTOP-2PGRMF4>
2023-03-21 17:20:52 +0900

    자바연습

--- 230321/src/arrayEx/ArrayEx15.java
+++ 230321/src/arrayEx/ArrayEx15.java
@@ -1,10 +1,19 @@
 package arrayEx;
 
+import java.util.Arrays;
 
 public class ArrayEx15 {
 
 	
 	public static void main(String[] args) {
+		String[] names =
+			{"홍길동","뉴진스","아이유","코요테","빅뱅"};
+		
+		for(String s : names) {
+			System.out.println("이름:"+s);
+			
+		
+		}
 		
 		
 		
