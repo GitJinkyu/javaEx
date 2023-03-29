6af8d95f81698bed402001c4efcfe5aadd8ef978
user <user@DESKTOP-2PGRMF4>
2023-03-29 09:46:48 +0900

    자바 연습

--- 230328/src/com/kh/practice2/Application.java
+++ 230328/src/com/kh/practice2/Application.java
@@ -12,14 +12,15 @@ public class Application {
 		
 		// 각 인덱스에 무작위로 Dog객체 또는 Cat객체로 생성
 		// (이때, 매개변수 생성자를 이용하여 생성)
+		Dog dog = new Dog("바둑이","똥개",30);
+		Cat cat = new Cat("나비", "스핑크스", "서울", "갈색");
+		
 		for(int i = 0; i<animalArr.length; i++) {
 			int dice =(int)(Math.random()*2+1);
 			
 			if(dice == 1) {
-				Dog dog = new Dog("바둑이","포유류",30);
 				animalArr[i] = dog;
 			}else {
-				Cat cat = new Cat("나비", "포유류", "서울", "갈색");
 				animalArr[i] = cat;
 			}
 		}
@@ -27,6 +28,10 @@ public class Application {
 		//향상된 for문으로 배열의 0번 인덱스부터 마지막 인덱스까지의
 		//객체의 speak() 메소드 호출 
 		for(Animal animalList : animalArr) {
+			//부모객체의 메서드를 호출했을때,
+			//부모객체의 메서드가 추상 메서드이고
+			//자식객체에서 오버라이딩(재정의)했을때
+			//자식객체의 메서드가 호출된다.
 			animalList.speak();
 		}
 	}
