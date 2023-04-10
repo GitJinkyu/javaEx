48f13bc7e08b22135bcafa4f9dd1d38c40f206ca
user <user@DESKTOP-2PGRMF4>
2023-04-10 16:50:58 +0900

    자바 연습

---
+++ jarex/src/jarex/main.java
@@ -0,0 +1,13 @@
+package jarex;
+
+import com.library.controller.LibraryController;
+
+public class main {
+
+	public static void main(String[] args) {
+		LibraryController controller= new LibraryController();
+		
+		controller.start();
+	}
+
+}
